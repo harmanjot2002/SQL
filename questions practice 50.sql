d6db6b1c88e6b004d8d999e15cea7d8e92917981
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-24 13:44:33 +0530

    50 practice questions of SQL

--- questions practice 50.sql
+++ questions practice 50.sql
@@ -1,3 +1,4 @@
+use org;
 -- Q-1. Write an SQL query to fetch “FIRST_NAME” from Worker table using the alias name as <WORKER_NAME>.
 select first_name AS WORKER_NAME from worker;
 
@@ -6,9 +7,27 @@ select UPPER(first_name) from worker;
 
 -- Q-3. Write an SQL query to fetch unique values of DEPARTMENT from Worker table.
 SELECT distinct department from worker;
+-- OR Group by without using aggregate functions also work as distinct.
+select department from worker group by department;
 
 -- Q-4. Write an SQL query to print the first three characters of  FIRST_NAME from Worker table.
 select substring(first_name, 1, 3) from worker;
 
 -- Q-5. Write an SQL query to find the position of the alphabet (‘b’) in the first name column ‘Amitabh’ from Worker table.
 select INSTR(first_name,'B') from worker where first_name = 'Amitabh';
+
+-- Q-6. Write an SQL query to print the FIRST_NAME from Worker table after removing white spaces from the right side.
+select RTRIM(first_name) from worker;
+
+-- Q-7. Write an SQL query to print the DEPARTMENT from Worker table after removing white spaces from the left side.
+select LTRIM(first_name) from worker;
+
+-- Q-8. Write an SQL query that fetches the unique values of DEPARTMENT from Worker table and prints its length.
+select distinct department, LENGTH(department) from worker;
+
+-- Q-9. Write an SQL query to print the FIRST_NAME from Worker table after replacing ‘a’ with ‘A’.
+select REPLACE(first_name, 'a', 'A')  from worker;
+
+-- Q-10. Write an SQL query to print the FIRST_NAME and LAST_NAME from Worker table into a single column COMPLETE_NAME.
+-- A space char should separate them.
+select CONCAT(first_name, ' ', last_name) AS COMPLETE_NAME from worker;
