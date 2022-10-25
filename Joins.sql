4fc9014dc1376969fe2c4932f561c2ef4eaf4379
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-25 13:08:49 +0530

    Joins

---
+++ Joins.sql
@@ -0,0 +1,11 @@
+/*
+	Joins:
+		1.Inner Join
+        2.Outer Join:
+			a)Left Join
+            b)Right Join
+            c)Full Join
+		3.Cross Join
+        4.Self Join
+        5.Join without using "join" keyword
+*/
