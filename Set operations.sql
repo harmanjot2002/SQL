00fd86b33a17c45c1d60eb8d1b9a9e7e06e240b7
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-25 13:22:06 +0530

    Set Operations

---
+++ Set operations.sql
@@ -0,0 +1,7 @@
+/*
+	Similar to joins but combines vertically.
+    No. of columns selected must be same from each table.
+	1.Union
+    2.Intersect(Emulated in MySQL using "INNER JOIN")
+    3.Minus(Emulated in MySQL using "LEFT JOIN")
+*/
