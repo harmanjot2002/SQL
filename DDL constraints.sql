d850860afc6848b9b334505b3a60c30cf4bad13a
harmanjot2002 <harmanjotkainth@gmail.com>
2022-10-25 09:56:35 +0530

    DDL Constraints

--- DDL constraints.sql
+++ DDL constraints.sql
@@ -6,3 +6,22 @@
             4.Default
             5.Unique            
 */
+
+use temp;
+CREATE TABLE account(
+	id int primary key,
+    name varchar(255) unique,
+    balance int NOT NULL default 200000,
+	constraint acc_balance_chk check(balance>1000)
+);
+
+-- DROP TABLE account;
+
+/*
+INSERT INTO account(id,name,balance) values(1,'A',1000);
+Above query output says that check constraint is violated. 
+*/
+
+INSERT INTO account(id,name,balance) values(1,'A',10000);
+INSERT INTO account(id,name) values(2,'B');
+SELECT * from account;
